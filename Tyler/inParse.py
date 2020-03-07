f560f5e6218a2a38d0bec297b8adb09477560132
Tyler Feist <tfeist612@gmail.com>
2020-03-06 19:42:28 -0700

    Added Or gate consistency function

--- Tyler/inParse.py
+++ Tyler/inParse.py
@@ -2,6 +2,28 @@ import sys
 import re
 
 
+def orGateConsistency(inList):
+    # Output is z
+    outputStr1 = ""
+    outputStr2 = "("
+    for a in range(0, len(inList)):
+        # Invert input for product section of equation
+        if inList[a][0] == "~":
+            inList[a] = inList[a][1:]
+        else:
+            inList[a] = "~" + inList[a]
+
+        if a == len(inList) - 1:       # Last one
+            outputStr1 += "(" + inList[a] + " + z)"
+            outputStr2 += inList[a] + " + ~z)"
+        else:
+            outputStr1 += "(" + inList[a] + " + z) * "
+            outputStr2 += inList[a] + " + "
+
+        output = outputStr1 + " * " + outputStr2
+    return output
+
+
 # Extracts expression side of an equation
 def extractExpression(equation):
     sides = re.split("[=]", equation)
@@ -54,8 +76,8 @@ if __name__ == '__main__':
 
     # Take user input
     print("Input a valid boolean function and include the output variable")
-    uIn = input()
-    # uIn = "~ab . (ab + cd) = h"
+    # uIn = input()
+    uIn = "~ab . (ab + cd) = h"
     print("Input was: " + uIn)
 
     checkFunction(uIn)
@@ -65,8 +87,13 @@ if __name__ == '__main__':
     v = extractVariables(e)
     c = countVariables(v)
 
+    li = ['~a', 'b', 'c']
+    x = orGateConsistency(li)
+    print(x)
+
+
     # To-Do
     # Need to parse the expression and format so we can use the gate functions to create a POS
-    # Implement gate functions
+    # Implement gate consistency functions
     # Pass parse functions to get POS output
     # Ensure output is readable for Devon's script
