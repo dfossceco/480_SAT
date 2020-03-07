fb90ac3a975a4b821946313e38852c5387384a81
dfossceco <dfossceco@gmail.com>
2020-03-07 11:30:43 -0700

    Modularising DPLL algorithm in single file.

--- devon/start_over/dpll.rb
+++ devon/start_over/dpll.rb
@@ -1,5 +1,5 @@
+# Recursive DPLL algorithm 
   def dpll(fxn)
-
     # Check if fxn is consistent (all 1's)
     if (fxn.all?('1'))
       return true
@@ -10,7 +10,16 @@
       return false
     end
 
-    # Unit propagation
+    # Unit propagation & simplification
+    fxn = simplify(fxn, unit_prop(fxn))
+    dpll(fxn)
+  end # def
+
+# UNIT PROPAGATION
+# Identify any unit clauses in the function
+# Make a hash of assignments based on the unit clauses
+# and return those assignments
+  def unit_prop (fxn)
     unit_clauses = []
     fxn.each do |term|
       if ( (term.length == 1 && !term.eql?('1') && !term.eql?('0')) || 
@@ -19,7 +28,51 @@
         unit_clauses.append(term) 
       end
     end # each do
-    fxn = unit_prop(fxn, unit_clauses)
-    dpll(fxn)
+    assignments = {}
+    unit_clauses.each do |unit_clause|
+      if (unit_clause.length == 2)
+        assignments[unit_clause.delete('~')] = '0'
+      else 
+        assignments[unit_clause] = '1'
+      end # if else
+    end # each do
+    return assignments
+  end # def 
 
+# SIMPLIFY
+# Given the function and hash of assignments,
+# simplify the function and return that simplified
+# function
+  def simplify (fxn, assignments)
+    assignments.each do |unit, value|
+      fxn.length.times do |index|
+        if (fxn[index].include?(unit))
+          if (fxn[index].length == 1)
+            fxn[index] = value
+          elsif (fxn[index].length == 2)
+            if (value.eql?('0'))
+              fxn[index] = '1'
+            else
+              fxn[index] = '0'
+            end # if else
+          else
+            term = fxn[index].split('+')
+            if ( (term.include?(unit) && value.eql?('1')) ||
+                 (term.include?('~'.concat(unit)) && value.eql?('0'))
+                )
+              fxn[index] = '1'
+            elsif (term.include?(unit) && value.eql?('0'))
+              term.delete_at(term.index(unit))
+              fxn[index] = term.join('+')
+            elsif (term.include?('~'.concat(unit)) && value.eql?('1'))
+              term.delete_at(term.index('~'.concat(unit)))
+              fxn[index] = term.join('+')
+            else
+              abort ('Something has gone horribly wrong!')
+            end # if elsif
+          end # if else
+        end # if 
+      end # times do
+    end # each do
+    return fxn
   end # def
--- devon/start_over/main.rb
+++ devon/start_over/main.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 require "./dpll.rb"
-require "./unit_prop.rb"
 
 # Get this shit from the file
 fxn = "h.a+d.b+d.~a+~b+~d.~b+~e.~c+~e.b+c+e.~d+~f.d+f.~d+g.~e+g.d+e+~g.f+~h.g+~h.~f+~g+h"
