a6a65bff62be0391d021eec872c9ba938809ac7e
dfossceco <dfossceco@gmail.com>
2020-03-10 11:07:46 -0600

    Adding multi-fxn to top level

--- devon/recursive_dpll/top.rb
+++ devon/recursive_dpll/top.rb
@@ -4,9 +4,29 @@ require "./dpll_obj.rb"
 # Select between the different modes of the SAT tool
 # Partial assignment : Get user input and determine SAT for those values 
 # Complete assignment : Automatically find assignments that will SAT fxn
-# TODO : Multi-fxn 
+# Multi : Determine if two networks are equivalent
 # If fxn is unSAT, return all assignments that led to this conclusion
 # If fxn is SAT, return all assignments that led to this conclusion
+
+# GET FUNCTION
+# Read the function and the list of inputs from the file
+ def get_fxn()
+  fxn_file = File.open("fxn.txt", "r")
+  fxn = ''
+  inputs = ''
+  i = 0
+  fxn_file.each do |line|
+    if (i == 0)
+      fxn = line.chomp.split('.')
+    else
+      inputs = line.chomp.split(',')
+    end # if else
+    i += 1
+  end # each do
+  fxn_file.close
+  # system("rm fxn.txt")
+  return fxn, inputs
+ end # def
 
 # COMPLETE
 # TODO: Let Tyler's script run and have it write to fxn.txt
@@ -14,20 +34,9 @@ require "./dpll_obj.rb"
 # Second line will contain the list of inputs
 # TODO: Who is going to print the CNF form?
   def complete()
-    # system ("./inParse.py")
-    fxn_file = File.open("fxn.txt", "r")
-    fxn = ''
-    inputs = ''
-    fxn_file.each do |line|
-      if (i == 0)
-        fxn = line.chomp.split('.')
-      else
-        inputs = line.chomp.split(',')
-      end # if else
-    end # each do
-    fxn_file.close
-    # system("rm fxn.txt")
-    return fxn, inputs
+    # user_in = gets().chomp
+    # system ("./inParse.py #{user_in}")
+    return get_fxn()
   end # def
 
 # PARTIAL
@@ -45,6 +54,30 @@ require "./dpll_obj.rb"
     end # times do
     return dpll.simplify(fxn, partial_assign)
   end #def
+
+# MULTI
+# Take in 2 functions
+# Produce the XOR of those two functions 
+# Pass that result to Tyler's script to get CNF
+# Return the CNF result
+  def multi()
+    puts('Input your first function:')
+    fxn1 = gets().chomp
+    puts('Input your second function:')
+    fxn2 = gets().chomp
+
+    fxn1 = '('.concat(fxn1).concat(')')
+    fxn2 = '('.concat(fxn2).concat(')')
+    not_fxn1 = '~'.concat(fxn1)
+    not_fxn2 = '~'.concat(fxn2)
+
+    term_a = '('.concat(fxn1).concat('.').concat(not_fxn2).concat(')')
+    term_b = '('.concat(not_fxn1).concat('.').concat(fxn2).concat(')')
+
+    fxn = term_a.concat('+').concat(term_b)
+    # system ("./inParse.py #{fxn}")
+    return get_fxn()
+  end # def
 
 # REDUCE
 # The function given in CNF may not be in terms of only the inputs
@@ -82,7 +115,12 @@ require "./dpll_obj.rb"
     if (run_type.eql?('partial'))
       fxn = partial(fxn, dpll)
     end # if
-  end # if
+  elsif (run_type.eql?('multi'))
+    fxn = multi()
+  else
+    abort ('INPUT MODE WAS NOT RECOGNIZED!')
+  end # if else
+
 
   status = dpll.dpll_rec(fxn)
   assignments = dpll.get_assignments()
